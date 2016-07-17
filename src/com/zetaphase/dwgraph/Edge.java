d223a01528c2e9a2fb0e15705496d6dc25d535ab
Dave Ho <daveho8888@gmail.com>
2016-07-17 16:48:53 -0700

    Node implementation finished
    also added parametrized stuff

--- src/com/zetaphase/dwgraph/Edge.java
+++ src/com/zetaphase/dwgraph/Edge.java
@@ -2,33 +2,28 @@ package com.zetaphase.dwgraph;
 
 import com.zetaphase.dwgraph.Node;
 
-public class Edge {
+public class Edge<T> {
 
-	private Node node1;
-	private Node node2;
+	private Node<T> node1;
+	private Node<T> node2;
 	
 	private int weight;
 	
-	public Edge(Node node1, Node node2, int weight){
+	public Edge(Node<T> node1, Node<T> node2, int weight){
 		this.node1 = node1;
 		this.node2 = node2;
 		this.weight = weight;
 	}
 	
-	public Node fromNode(){
+	public Node<T> fromNode(){
 		return node1;
 	}
 	
-	public Node toNode(){
+	public Node<T> toNode(){
 		return node2;
 	}
 	
-	public boolean isBetween(Node node1, Node node2){
+	public boolean isBetween(Node<T> node1, Node<T> node2){
 		return (this.node1==node1 && this.node2==node2);
 	}
-	
-	public Edge() {
-		// TODO Auto-generated constructor stub
-	}
-
 }
