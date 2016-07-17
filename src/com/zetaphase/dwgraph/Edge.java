b565ebe38d388e3647c2f0336af43b40bd21c981
Dave Ho <daveho8888@gmail.com>
2016-07-17 16:32:45 -0700

    edge.java implemented

--- src/com/zetaphase/dwgraph/Edge.java
+++ src/com/zetaphase/dwgraph/Edge.java
@@ -1,7 +1,32 @@
 package com.zetaphase.dwgraph;
 
+import com.zetaphase.dwgraph.Node;
+
 public class Edge {
 
+	private Node node1;
+	private Node node2;
+	
+	private int weight;
+	
+	public Edge(Node node1, Node node2, int weight){
+		this.node1 = node1;
+		this.node2 = node2;
+		this.weight = weight;
+	}
+	
+	public Node fromNode(){
+		return node1;
+	}
+	
+	public Node toNode(){
+		return node2;
+	}
+	
+	public boolean isBetween(Node node1, Node node2){
+		return (this.node1==node1 && this.node2==node2);
+	}
+	
 	public Edge() {
 		// TODO Auto-generated constructor stub
 	}
